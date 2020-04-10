b60b367376fc25cc8a29af3e51bce367c69d2e86
Luiz Gonzaga <luizgonzaga15@gmail.com>
2020-04-10 01:14:36 -0300

    Começando a criar os métodos do CRUD.

--- DAL/Persistence/PessoaDAL.cs
+++ DAL/Persistence/PessoaDAL.cs
@@ -15,16 +15,42 @@ namespace DAL.Persistence
 			try
 			{
 				AbrirConexao();
+				Cmd = new SqlCommand("insert into Pessoa(Nome, Endereco, Email) values (@v1, @v2, @v3)", Con);
+
+				Cmd.Parameters.AddWithValue("@v1", pessoa.Nome);
+				Cmd.Parameters.AddWithValue("@v2", pessoa.Endereco);
+				Cmd.Parameters.AddWithValue("@v3", pessoa.Email);
+
+				Cmd.ExecuteNonQuery();
 			}
 			catch (Exception ex)
 			{
 
-				throw;
+				throw new Exception("Erro ao gravar a pessoa:" + ex.Message);
 			}
 			finally
 			{
 				FecharConexao();
 			}
         }
+
+		public void Atualizar(Pessoa pessoa)
+		{
+			try
+			{
+				AbrirConexao();
+
+			}
+			catch (Exception ex)
+			{
+
+				throw new Exception("Erro ao atualizar a pessoa: "+ ex.Message);
+			}
+			finally
+			{
+				FecharConexao();
+			}
+		}
+
     }
 }
